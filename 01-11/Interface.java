74e624d294324ad5d025752eb44f2cdf7d1c5fcd
beabritoliveira <103044603+beabritoliveira@users.noreply.github.com>
2022-11-01 10:06:55 -0300

    Update Interface.java

--- 01-11/Interface.java
+++ 01-11/Interface.java
@@ -1,1 +1,2 @@
-0
+//        Como não é possível no Java a utilização de Herança Multipla de classes, assim, se utiliza interface para que haja conecção de uma classe herdando de 2 ou mais 
+//        Não utiliza mais o EXTENDS e agora também um IMPLEMENT -> dentro dela só tem a assinatura dos metodos e não a sua construção
